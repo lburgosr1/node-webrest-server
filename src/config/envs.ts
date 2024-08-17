a4f37f96c9186b233a0f103a8e260f7408cff00d
Luis Burgos <luismanuelbr.2007@gmail.com>
2024-08-17 15:17:34 -0400

    included prisma and postgreDB

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,5 +4,6 @@ import { get } from 'env-var';
 export const envs = {
 
     PORT: get('PORT').required().asPortNumber(),
+    POSTGRES_URL: get('POSTGRES_URL').required().asString(),
     PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
 }
